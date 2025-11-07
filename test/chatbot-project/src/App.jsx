4cc3259e82abed64b819033c8ddc52fabcd838e6
MayanthaS <sapumalmayantha@gmail.com>
2025-11-08 00:00:16 +0530

    updated file

--- test/chatbot-project/src/App.jsx
+++ test/chatbot-project/src/App.jsx
@@ -1,56 +1,12 @@
-import { useState,useRef,useEffect } from 'react'
+import { useState } from 'react'
 import {ChatInput} from './components/Chatinput'
-import RobbotProfileImage from './assets/robot.png';
-import UserProfileImage from './assets/user.png';
+
 import './App.css'
 
  
 
-      function ChatMessage({ message, sender }) {
-        return (
-          <div className={
-            sender === 'user'? 'chat-message-user'
-            :'chat-message-robot'
-           }>
-            {sender === 'robot' && <img src={RobbotProfileImage}
-              className="chat-message-profile"
-            />}
-            <div className ="chat-message-text">
-               {message}
-            </div>
-           
-            {sender === 'user' && <img src={UserProfileImage}
-             className="chat-message-profile"
-            />}
-          </div>
-        );
-      }
-
-      function ChatMessages({ chatMessages }) {
-        const chatMessagesRef =useRef(null);
-        useEffect(() =>{
-          const containerElem = chatMessagesRef.current;
-          if(containerElem){
-            containerElem.scrollTop =containerElem.scrollHeight;
-
-          }
-           },[chatMessages]);
-        return (
-          <div 
-           className="chat-message-container"
-           ref={chatMessagesRef}
-          >
-            {chatMessages.map((msg) => (
-              <ChatMessage
-                key={msg.id}
-                message={msg.message}
-                sender={msg.sender}
-              />
-            ))}
-          </div>
-        );
-      }
-
+     
+    
 
 function App() {
         const [chatMessages, setChatMessages] = useState([
---
+++ test/chatbot-project/src/components/ChatMessges.jsx
@@ -0,0 +1,27 @@
+  import { useEffect,useRef } from "react";
+  import { ChatMessage } from "./ChatMessage";
+
+  export function ChatMessages({ chatMessages }) {
+        const chatMessagesRef =useRef(null);
+        useEffect(() =>{
+          const containerElem = chatMessagesRef.current;
+          if(containerElem){
+            containerElem.scrollTop =containerElem.scrollHeight;
+
+          }
+           },[chatMessages]);
+        return (
+          <div 
+           className="chat-message-container"
+           ref={chatMessagesRef}
+          >
+            {chatMessages.map((msg) => (
+              <ChatMessage
+                key={msg.id}
+                message={msg.message}
+                sender={msg.sender}
+              />
+            ))}
+          </div>
+        );
+      }
